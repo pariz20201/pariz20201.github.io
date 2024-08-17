24aaf5dc477470ca746ee1a46a6ff71254f0ecc2
Pariss <parisgranados91@gmail.com>
2024-08-16 19:54:36 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//console.log("Hola Mundo!");
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Paris Abril Granados Borraz')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
